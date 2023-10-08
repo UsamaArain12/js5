5f8a5a3cf630e8d0878488e3d589c28710d9cc87
Usama Arain <ua339716@gmail.com>
2023-10-07 23:16:10 -0700

    js5

---
+++ appp.js
@@ -0,0 +1,128 @@
+var userName =  prompt ("Enter Usama")
+if (userName == 'Usama') {
+   alert("wellcome my website") 
+}
+
+
+var a = "Sum of 3 and 5 is 8"
+document.write (a)
+document.write ("<br>")
+document.write ("<br>")
+
+var b = 3 + 5
+console.log(b)
+
+var c = 3 * 5
+console.log(c)
+
+var d = 3 - 5
+console.log(d)
+
+var e = 5 % 3
+console.log(e)
+
+
+var f = "5 + 5 ?"
+document.write(f)
+document.write ("<br>")
+document.write ("<br>")
+
+var g = 5
+var h = ++g
+var h2 = g--
+var h3 = --g
+var h4 = ++g
+var h5 = g++
+document.write(g)
+document.write ("<br>")
+document.write ("<br>")
+
+var gg = 5
+var hh = g++
+document.write(gg)
+document.write ("<br>")
+document.write ("<br>")
+
+
+var g1 = 5
+var h1 = --g
+document.write(h1)
+document.write ("<br>")
+document.write ("<br>")
+
+var j = 5;
+var j1 = 8;
+var j2 = j + j1;
+document.write(j2)
+document.write ("<br>")
+document.write ("<br>")
+
+var z = 13
+var z1 = --z
+document.write(z)
+document.write ("<br>")
+document.write ("<br>")
+
+var total = 600 * 5
+document.write(`<h1>Total cost to buy 5 tickets to a bus is ${total} </h1>`)
+document.write ("<br>")
+document.write ("<br>")
+
+var table = prompt ('Enter Table Number')
+document.write('Table of ' + table)
+document.write('<br>')
+document.write(table + 'x' + 1 + '=' + table*1)
+document.write('<br>')
+document.write(table + 'x' + 2 + '=' + table*2)
+document.write('<br>')
+document.write(table + 'x' + 3 + '=' + table*3)
+document.write('<br>')
+document.write(table + 'x' + 4 + '=' + table*4)
+document.write('<br>')
+document.write(table + 'x' + 5 + '=' + table*5)
+document.write('<br>')
+document.write(table + 'x' + 6 + '=' + table*6)
+document.write('<br>')
+document.write(table + 'x' + 7 + '=' + table*7)
+document.write('<br>')
+document.write(table + 'x' + 8 + '=' + table*8)
+document.write('<br>')
+document.write(table + 'x' + 9 + '=' + table*9)
+document.write('<br>')
+document.write(table + 'x' + 10 + '=' + table*10)
+document.write('<br>')
+document.write('<br>')
+
+
+var k = 10
+var k1 = k * 3
+var k2 = 20
+var k3 = k2 * 5
+var k4 = k1 + k3
+document.write(`<p>item one cost is 10 <br> item two cost is 20 <br> item one quantity 3 <br> item two quantity 5 <br> Total amount is  <b>${k4}</b> </p>`)
+
+var t = 600
+var t1 = 480
+var to = ( (t1/t) * 100 )
+document.write(`<p> Total Marks ${t} <br> Obtained Marks ${t1} <br> Percentage is ${to} </p>`)
+
+var d = 18
+var e = 40
+var d1 = d * 288
+var e1 = e * 320
+var e2 = d1 + e1
+document.write(`<p>Dollar : ${d} <br> Euro : ${e} <br> Dollar Rate is 288:PKR <br> Euro Rate is 320:PKR <br> Totoal in PKR <b>${e2}</b> </p>`)
+
+var k = 10 + 5 * 10 / 2
+console.log(k)
+
+
+var cu = 2023
+var cu1 = 1998
+var cu2 = cu - cu1
+document.write(`<p> Current Year ${cu} <br> Birth Year ${cu1} <br> Your Age is <b>${cu2}</b> </p>`)
+
+
+
+
+ 
